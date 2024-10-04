0bdb83e7b599cc0c0d0836784ccc6ccf6627391e
ftrmnn <fr9299540@gmail.com>
2024-10-04 19:54:43 +0700

    update responsive button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = () => {
     return (
         <div className="flex justify-center">
-            <button className="py-3 items-center justify-center border-4 hover:bg-green-600 hover:text-white px-36 text-green-900 bg-white border border-green-600 border-spacing-3">
+            <button className="py-3 items-center justify-center border-4 hover:bg-green-600 hover:text-white md:px-36 px-14 text-green-900 bg-white border border-green-600 border-spacing-3">
             Booking CV
             </button>
         </div>
