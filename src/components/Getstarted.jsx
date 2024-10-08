044c2b4196cdebed671a4f785c90b71cec0c387a
ftrmnn <fr9299540@gmail.com>
2024-10-08 23:08:34 +0700

    update input order new

--- src/components/Getstarted.jsx
+++ src/components/Getstarted.jsx
@@ -23,18 +23,22 @@ const Modal = ({ isOpen, onClose, serviceTitle, servicePrice }) => {
     const [education, setEducation] = useState('');
     const [skills, setSkills] = useState('');
     const [selectedService, setSelectedService] = useState(serviceTitle);
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
     const [showConfirmation, setShowConfirmation] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const message = `Haii Admin,\n\nSelamat Siang! Saya sangat bersemangat untuk memulai perjalanan karier saya dan ingin memesan jasa pembuatan CV. Berikut detail yang ingin saya sertakan:\n\n*Nama Lengkap:* ${name}\n*Layanan yang Dipilih:* ${selectedService}\n*Harga yang Harus Dibayar:* ${servicePrice}\n*Posisi yang Dilamar:* ${position}\n*Pengalaman Kerja:* ${experience}\n*Pendidikan Terakhir:* ${education}\n*Keahlian Khusus:* ${skills}\n\nJika ada format atau informasi tambahan yang diperlukan, mohon informasikan. Terima kasih atas bantuannya! Semoga kita bisa menciptakan CV yang luar biasa bersama.`;
+        const message = `Haii Admin,\n\nSelamat Siang! Saya sangat bersemangat untuk memulai perjalanan karier saya dan ingin memesan jasa pembuatan CV. Berikut detail yang ingin saya sertakan:\n\n*Nama Lengkap:* ${name}\n*Layanan yang Dipilih:* ${selectedService}\n*Harga yang Harus Dibayar:* ${servicePrice}\n*Nomor Telepon:* ${phone}\n*Email:* ${email}\n*Posisi yang Dilamar:* ${position}\n*Pengalaman Kerja:* ${experience}\n*Pendidikan Terakhir:* ${education}\n*Keahlian Khusus:* ${skills}\n\nJika ada format atau informasi tambahan yang diperlukan, mohon informasikan. Terima kasih atas bantuannya! Semoga kita bisa menciptakan CV yang luar biasa bersama.`;
         const whatsappUrl = `https://wa.me/6285878954625?text=${encodeURIComponent(message)}`;
         window.open(whatsappUrl, '_blank');
         setName('');
+        setPhone('');
         setPosition('');
         setExperience('');
         setEducation('');
         setSkills('');
+        setEmail('');
         onClose();
         setShowConfirmation(true);
         setTimeout(() => setShowConfirmation(false), 10000);
@@ -42,8 +46,8 @@ const Modal = ({ isOpen, onClose, serviceTitle, servicePrice }) => {
 
     return (
         <div>
-            <div className={`fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center ${isOpen ? 'block' : 'hidden'}`} onClick={onClose}>
-                <div className="bg-white z-50 p-5 rounded-lg shadow-lg w-11/12 md:w-1/3 max-w-lg" onClick={(e) => e.stopPropagation()}>
+            <div className={`fixed inset-0 z-50 bg-black bg-opacity-50 flex justify-center items-center ${isOpen ? 'block' : 'hidden'}`} onClick={onClose}>
+            <div className="bg-white z-50 p-5 rounded-lg shadow-lg w-11/12 md:w-1/3 max-w-lg" onClick={(e) => e.stopPropagation()}>
                     <h2 className="text-lg font-semibold mb-2">Format Order</h2>
                     <form onSubmit={handleSubmit}>
                         <button className="absolute top-2 right-2 text-xl" onClick={onClose}>×</button>
@@ -70,47 +74,73 @@ const Modal = ({ isOpen, onClose, serviceTitle, servicePrice }) => {
                                 <option value="CV.Ku Custom">CV.Ku Custom</option>
                             </select>
                         </div>
-                        <div className="mb-4">
-                            <label className="block mb-1">Posisi yang Dilamar:</label>
-                            <input 
-                                type="text" 
-                                value={position} 
-                                onChange={(e) => setPosition(e.target.value)} 
-                                className="border rounded w-full p-2"
-                                required
-                            />
+                        <div className="flex flex-col md:flex-row md:space-x-4 mb-4">
+                            <div className="md:w-1/2">
+                                <label className="block mb-1">Nomor Telepon:</label>
+                                <input 
+                                    type="tel" 
+                                    value={phone} 
+                                    onChange={(e) => setPhone(e.target.value)} 
+                                    className="border rounded w-full p-2"
+                                    required
+                                />
+                            </div>
+                            <div className="md:w-1/2">
+                                <label className="block mb-1">Email:</label>
+                                <input 
+                                    type="email" 
+                                    value={email} 
+                                    onChange={(e) => setEmail(e.target.value)} 
+                                    className="border rounded w-full p-2"
+                                    required
+                                />
+                            </div>
                         </div>
-                        <div className="mb-4">
-                            <label className="block mb-1">Pengalaman Kerja:</label>
-                            <input 
-                                type="text" 
-                                value={experience} 
-                                onChange={(e) => setExperience(e.target.value)} 
-                                className="border rounded w-full p-2"
-                                required
-                            />
+                        <div className="flex flex-col md:flex-row md:space-x-4 mb-4">
+                            <div className="md:w-1/2">
+                                <label className="block mb-1">Posisi yang Dilamar:</label>
+                                <input 
+                                    type="text" 
+                                    value={position} 
+                                    onChange={(e) => setPosition(e.target.value)} 
+                                    className="border rounded w-full p-2"
+                                    required
+                                />
+                            </div>
+                            <div className="md:w-1/2">
+                                <label className="block mb-1">Pengalaman Kerja:</label>
+                                <input 
+                                    type="text" 
+                                    value={experience} 
+                                    onChange={(e) => setExperience(e.target.value)} 
+                                    className="border rounded w-full p-2"
+                                    required
+                                />
+                            </div>
                         </div>
-                        <div className="mb-4">
-                            <label className="block mb-1">Pendidikan Terakhir:</label>
-                            <input 
-                                type="text" 
-                                value={education} 
-                                onChange={(e) => setEducation(e.target.value)} 
-                                className="border rounded w-full p-2"
-                                required
-                            />
+                        <div className="flex flex-col md:flex-row md:space-x-4 mb-4">
+                            <div className="md:w-1/2">
+                                <label className="block mb-1">Pendidikan Terakhir:</label>
+                                <input 
+                                    type="text" 
+                                    value={education} 
+                                    onChange={(e) => setEducation(e.target.value)} 
+                                    className="border rounded w-full p-2"
+                                    required
+                                />
+                            </div>
+                            <div className="md:w-1/2">
+                                <label className="block mb-1">Keahlian Khusus:</label>
+                                <input 
+                                    type="text" 
+                                    value={skills} 
+                                    onChange={(e) => setSkills(e.target.value)} 
+                                    className="border rounded w-full p-2"
+                                    required
+                                />
+                            </div>
                         </div>
-                        <div className="mb-4">
-                            <label className="block mb-1">Keahlian Khusus:</label>
-                            <input 
-                                type="text" 
-                                value={skills} 
-                                onChange={(e) => setSkills(e.target.value)} 
-                                className="border rounded w-full p-2"
-                                required
-                            />
-                        </div>
-                        <button type="submit" className="bg-green-600 text-white py-2 px-4 rounded">
+                        <button type="submit" className="bg-green-600 text-white py-2 px-4 w-full rounded">
                             Kirim
                         </button>
                     </form>
@@ -152,23 +182,27 @@ const Getstarted = () => {
     return (
         <div id="order" className="mx-4 md:mx-10 mt-10 md:mt-20 p-6 md:p-14 font-poppins">
             <h1 className="text-lg md:text-2xl text-center text-green-900 font-semibold">Mari Mulai</h1>
-            <p className="text-center mx-auto max-w-xs md:max-w-lg pt-4">Klik di sini untuk menambahkan teks Anda sendiri dan sesuaikan sesuai kebutuhan dalam penulisan CV Anda.</p>
-            <Line />
-            {services.map((service, index) => (
-                <div key={index}>
+            <p className="text-center mx-auto max-w-xs md:max-w-lg text-gray-700 mt-2">Pesan layanan pembuatan CV dan surat lamaran yang siap saing untuk memudahkan anda dalam mencari pekerjaan impian.</p>
+            <div className="flex flex-col space-y-4 mt-8">
+                {services.map((service, index) => (
                     <Getstartd 
+                        key={index}
                         title={service.title} 
-                        price={service.price} 
+                        price={service.price}
                         onOpenModal={() => {
                             setSelectedService(service.title);
                             setSelectedPrice(service.price);
                             setIsModalOpen(true);
-                        }} 
+                        }}
                     />
-                    <Line />
-                </div>
-            ))}
-            <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} serviceTitle={selectedService} servicePrice={selectedPrice} />
+                ))}
+            </div>
+            <Modal 
+                isOpen={isModalOpen} 
+                onClose={() => setIsModalOpen(false)} 
+                serviceTitle={selectedService} 
+                servicePrice={selectedPrice}
+            />
         </div>
     );
 };
