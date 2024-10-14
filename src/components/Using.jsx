2b430ae692a49f96f765c8010edf75e5c97f4db6
siddiqfathurahman <fr9299540@gmail.com>
2024-10-14 21:26:02 +0700

    update using layanan

--- src/components/Using.jsx
+++ src/components/Using.jsx
@@ -6,16 +6,16 @@ const Using = () => {
     const [counterOn, setCounterOn] = useState(false);
 
     return (
-        <div className="h-screen flex flex-col justify-center text-center items-center">
+        <div className="h-screen flex flex-col justify-center text-center items-center font-poppins">
             <ScrollTrigger
                 onEnter={() => setCounterOn(true)}
                 onExit={() => setCounterOn(false)}
             >
                 <h1 className="text-9xl font-extrabold text-green-900">
-                    {counterOn && <CountUp start={0} end={10000} duration={2} delay={0} />} +
+                    {counterOn && <CountUp start={0} end={5362} duration={2} delay={0} />}
                 </h1>
-                <h2 className="text-5xl pt-4 font-bold text-center max-w-4xl">
-                    uses fahost for their hosting and servers
+                <h2 className="text-5xl pt-4 font-bold text-center max-w-4xl leading-snug">
+                Dipercaya oleh ribuan pengguna untuk layanan CV profesional
                 </h2>
             </ScrollTrigger>
         </div>
